bae2d243499a2ffad25bcfaba0704efeaa61c3fb
Ben <rapiidz1@gmail.com>
2018-10-18 14:32:38 +0000

    player 1 can attack player 2

---
+++ spec/features/attack_spec.rb
@@ -0,0 +1,7 @@
+feature 'Attacking' do 
+   scenario 'player 1 attacks player 2' do 
+      sign_in_and_play
+      click_link 'Attack'
+      expect(page).to have_content 'Dave attacked Mittens'
+   end
+end
